4a2ce223d007eb81099da6f2ab1d7f16f3063595
Marius Damm <mariusdamm1010@gmail.com>
2025-08-13 15:33:07 +0200

    feat: add habamax colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("tokyonight").setup({
-    })
-    vim.cmd.colorscheme "tokyonight-night"
-  end
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("tokyonight").setup({
+      })
+      -- vim.cmd.colorscheme "tokyonight-night"
+    end
 
+  },
+  { 
+    "ntk148v/habamax.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      vim.cmd.colorscheme "habamax"
+    end,
+  },
 }
