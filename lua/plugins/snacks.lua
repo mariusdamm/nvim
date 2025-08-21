a82d7ae54a0152301a0197ee13b4c36676a72b80
Marius Damm <mariusdamm1010@gmail.com>
2025-08-21 21:11:24 +0200

    feat: add key for project list

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -32,5 +32,6 @@ return {
     { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
     { "<leader>bd", function(n) Snacks.bufdelete(n) end, desc = "Remove current buffer" },
     { "<leader>C", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
   },
 }
