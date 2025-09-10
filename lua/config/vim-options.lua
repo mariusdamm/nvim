2ab88da3497b3104b6ea95cd1e24e820fd20e9fa
Marius Damm <mariusdamm1010@gmail.com>
2025-09-10 14:58:12 +0200

    fix: add function to close buf and tab of lspinfo

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -9,3 +9,15 @@ vim.cmd("set shiftwidth=2")
 -- vim.g.mapleader = " "
 -- vim.g.maplocalleader = "\\"
 -- vim.keymap.set("n", "<leader>qq", ":wqa<CR>", { desc = "Write-Quit everything" })
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  callback = function(ev)
+    if vim.bo[ev.buf].buftype == "nofile" then
+      vim.schedule(function()
+        if vim.api.nvim_buf_is_valid(ev.buf) then
+          vim.api.nvim_buf_delete(ev.buf, { force = true })
+        end
+      end)
+    end
+  end,
+})
