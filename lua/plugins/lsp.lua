b581be2ac6870ce2ab45790e4542178fa2afb651
Marius Damm <mariusdamm1010@gmail.com>
2025-09-25 13:37:21 +0200

    feat: add inline diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,4 +48,13 @@ return {
       })
     end
   },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+      vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
+      vim.keymap.set("n", "<Leader>cl", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" }
+      )
+    end,
+  },
 }
