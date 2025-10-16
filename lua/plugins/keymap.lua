af00ddfec82a35c618a4e88baa62d783b95b31f3
Marius Damm <mariusdamm1010@gmail.com>
2025-10-16 11:39:00 +0200

    feat: update keymap
    Add L and H that scroll almost to the bottom / top of the screen

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -1,41 +1,44 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    'echasnovski/mini.nvim',
-  },
-  opts = {
-    preset = "helix",
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    -- {
-    --   "<leader>?",
-    --   function()
-    --     require("which-key").show({ global = false })
-    --   end,
-    --   desc = "Buffer Local Keymaps (which-key)",
-    -- },
-  },
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"echasnovski/mini.nvim",
+	},
+	opts = {
+		preset = "helix",
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		-- {
+		--   "<leader>?",
+		--   function()
+		--     require("which-key").show({ global = false })
+		--   end,
+		--   desc = "Buffer Local Keymaps (which-key)",
+		-- },
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
 
-    wk.add({
-      { "<leader>b", group = "buffer" },
-      { "<leader>g", group = "git" },
-      { "<leader>s", group = "search" },
-      { "<leader>f", group = "file" },
-      { "<leader>c", group = "code" },
-      { "<leader>t", group = "tabs" },
-      { "<leader>w", proxy = "<c-w>", group = "windows" },
-      { "<leader>qq", ":qa<CR>", desc = "Quit all", hidden = true},
-      { "<leader>tn", ":tabnext<CR>", desc = "Next tab" },
-      { "<leader>tp", ":tabprevious<CR>", desc = "Previous tab" },
-      { "<leader>tt", ":tabnew<CR>", desc = "New tab" },
-      { "<leader>tq", ":tabclose<CR>", desc = "Close tab" },
-    })
-  end
+		wk.add({
+			{ "<leader>b", group = "buffer" },
+			{ "<leader>g", group = "git" },
+			{ "<leader>s", group = "search" },
+			{ "<leader>f", group = "file" },
+			{ "<leader>c", group = "code" },
+			{ "<leader>t", group = "tabs" },
+			{ "<leader>w", proxy = "<c-w>", group = "windows" },
+			{ "<leader>qq", ":qa<CR>", desc = "Quit all", hidden = true },
+			{ "<leader>tn", ":tabnext<CR>", desc = "Next tab" },
+			{ "<leader>tp", ":tabprevious<CR>", desc = "Previous tab" },
+			{ "<leader>tt", ":tabnew<CR>", desc = "New tab" },
+			{ "<leader>tq", ":tabclose<CR>", desc = "Close tab" },
+		})
+
+		vim.keymap.set("n", "L", "L5<C-e>", { noremap = true, silent = true })
+		vim.keymap.set("n", "H", "H5<C-y>", { noremap = true, silent = true })
+	end,
 }
