4d7a55ee194c73a08e329f8b86ba1d3c0c158938
Marius Damm <mariusdamm1010@gmail.com>
2025-10-16 11:09:53 +0200

    feat: add colorscheme onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,29 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme "tokyonight-night"
-    end
-
-  },
-  {
-    "ntk148v/habamax.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    config = function()
-      -- vim.cmd.colorscheme "habamax"
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			-- vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+	{
+		"ntk148v/habamax.nvim",
+		dependencies = { "rktjmp/lush.nvim" },
+		config = function()
+			-- vim.cmd.colorscheme "habamax"
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				style = "darker",
+			})
+			-- Enable theme
+			require("onedark").load()
+		end,
+	},
 }
