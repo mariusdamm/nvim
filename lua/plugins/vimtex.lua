3171c33473e4bfaefed845dab96d4c4fc109020c
Marius Damm <mariusdamm1010@gmail.com>
2025-11-10 16:33:20 +0100

    style: format code

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,10 +3,10 @@ return {
 	lazy = false,
 	init = function()
 		-- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_enabled = 1
-    vim.g.vimtex_compiler_method = 'latexmk'
+		vim.g.vimtex_enabled = 1
+		vim.g.vimtex_compiler_method = "latexmk"
 		vim.g.vimtex_view_method = "skim"
-    vim.g.vimtex_view_automatic = 0
-    vim.g.vimtex_toc_displayed = 0
+		vim.g.vimtex_view_automatic = 0
+		vim.g.vimtex_toc_displayed = 0
 	end,
 }
