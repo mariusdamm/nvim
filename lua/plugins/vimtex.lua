dcf1642f09241839ab04eb2924bb1905684f8c5f
Marius Damm <mariusdamm1010gmail.com>
2025-12-08 08:54:23 +0100

    fix: change vimtex view method to zathura

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,7 +5,7 @@ return {
 		-- VimTeX configuration goes here, e.g.
 		vim.g.vimtex_enabled = 1
 		vim.g.vimtex_compiler_method = "latexmk"
-		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_view_automatic = 0
 		vim.g.vimtex_toc_displayed = 0
 	end,
