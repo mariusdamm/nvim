3664803a03348db144805e1a840c6ad5f202ea86
Marius Damm <mariusdamm1010gmail.com>
2025-12-08 08:54:34 +0100

    style: format copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,22 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = true },
-        panel = { enabled = true },
-        filetypes = {
-          go = true,
-          ["*"] = true, -- alles erlauben, ausser md
-          markdown = false,
-        },
-      })
-    end,
-  },
-  {
-    "giuxtaposition/blink-cmp-copilot",
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = true },
+				panel = { enabled = true },
+				filetypes = {
+					go = true,
+					["*"] = true, -- alles erlauben, ausser md
+					markdown = false,
+				},
+			})
+		end,
+	},
+	{
+		"giuxtaposition/blink-cmp-copilot",
+	},
 }
+
