8ccf80d6f7adcee133002540b6f7f1528ce14a58
Marius Damm <mariusdamm1010gmail.com>
2025-12-08 08:54:02 +0100

    fix: disable perl provider

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -5,19 +5,20 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("let g:loaded_perl_provider = 0")
 
 -- vim.g.mapleader = " "
 -- vim.g.maplocalleader = "\\"
 -- vim.keymap.set("n", "<leader>qq", ":wqa<CR>", { desc = "Write-Quit everything" })
 
 vim.api.nvim_create_autocmd("BufWinLeave", {
-  callback = function(ev)
-    if vim.bo[ev.buf].buftype == "nofile" then
-      vim.schedule(function()
-        if vim.api.nvim_buf_is_valid(ev.buf) then
-          vim.api.nvim_buf_delete(ev.buf, { force = true })
-        end
-      end)
-    end
-  end,
+	callback = function(ev)
+		if vim.bo[ev.buf].buftype == "nofile" then
+			vim.schedule(function()
+				if vim.api.nvim_buf_is_valid(ev.buf) then
+					vim.api.nvim_buf_delete(ev.buf, { force = true })
+				end
+			end)
+		end
+	end,
 })
